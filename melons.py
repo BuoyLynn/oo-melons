e618419a62377a3b75b800647bed635d714593bc
Your Name <you@example.com>
2020-02-07 12:39:25 -0800

    Implemented Part 3, GovernmentMelonOrder

--- melons.py
+++ melons.py
@@ -3,6 +3,7 @@
 
 class AbstractMelonOrder():
     def __init__(self, species, qty):
+        """Initializes an Abstract Melon Class."""
         self.species = species
         self.qty = qty
         self.shipped = False
@@ -13,6 +14,7 @@ class AbstractMelonOrder():
 
         base_price = 5
 
+        # Christmas Melons are more x1.5 expensive than other melons
         if self.species == "Christmas Melon":
             base_price = base_price * 1.5
 
@@ -28,7 +30,9 @@ class AbstractMelonOrder():
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
+
     def __init__(self, species, qty):
+        """Initialize melon order attributes."""
         super().__init__(species, qty)
         self.order_type = "domestic"
         self.tax = 0.08
@@ -53,6 +57,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return self.country_code
 
     def get_total(self):
+        """Updating get_total to add a flat $3 fee to intl orders with < 10 melons"""
         total = super().get_total()
         if self.qty < 10:
             total += 3.00
@@ -60,3 +65,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     # def __repr__(self):
     #     return f"{self.species}\n{self.qty}\n{self.shipped}\n{self.tax}\n{self.order_type}\n{self.country_code}"
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A government melon order. Govt orders are tax-exempt."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        super().__init__(species, qty)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Update a melon's inspection status with a Boolean."""
+        self.passed_inspection = passed
